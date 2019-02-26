68dd3269763333aa6f89ed3eadcee8ba762e3bf4
andrey <bezlea@rarus.ru>
2019-02-26 16:20:13 +0200

    сделал задание 16

--- 16.php
+++ 16.php
@@ -1,7 +1,12 @@
 <?php
 
 
-function getSameCount()
+function getSameCount($firstArr, $secondArr)
 {
-
+    return count(array_unique(array_intersect($firstArr, $secondArr)));
 }
+
+
+echo getSameCount([], []); // => 0
+echo getSameCount([1, 10, 3], [10, 100, 35, 1]); // => 2
+echo getSameCount([1, 3, 2, 2], [3, 1, 1, 2]); // => 3
