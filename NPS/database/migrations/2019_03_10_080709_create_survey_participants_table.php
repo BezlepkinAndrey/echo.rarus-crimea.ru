fa5eb338a4b5be1771c7426188c019239dd50f3b
andrey <bezlea@rarus.ru>
2019-03-10 11:14:18 +0200

    Начал работу с миграциями. Добавил две миграции - участник опроса и оценка

---
+++ NPS/database/migrations/2019_03_10_080709_create_survey_participants_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSurveyParticipantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('survey_participants', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('secret_key');
+            $table->string('tip');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('survey_participants');
+    }
+}
